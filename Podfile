dc35b135470b080bb3c938b1ce894a6e28454bf3
MACIOLEK Sebastian <ms613363@s202p06m.iutnice.unice.fr>
2017-02-20 13:50:28 +0100

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.2'
+
+target 'TD5’ do
+    use_frameworks!
+    pod 'SWXMLHash', '~> 3.0'
+    pod 'SDWebImage', '~> 4.0'
+end
---
+++ TD5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TD5
+//
+//  Created by MACIOLEK Sebastian on 20/02/2017.
+//  Copyright © 2017 MACIOLEK Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
